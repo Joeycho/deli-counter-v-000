584c59a73acd31b610db727a7be7e01fcdf9ef8d
Joey Cho <cjy12182000@naver.com>
2017-10-25 10:56:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     counter = 1
-      puts "The line is currently:"
+      tempstr = "The line is currently:"
     katz_deli.each do |person|
-      puts "#{counter}. "
-      puts "#{person}"
+      tempstr += "#{counter}. "
+      tempstr += "#{person}"
       counter +=1
     end
+      puts tempstr
   end
 end
