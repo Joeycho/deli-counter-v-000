8e44fbd38e14305b7250d49fd2d12021df586ced
Joey Cho <cjy12182000@naver.com>
2017-10-25 10:54:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     counter = 1
+      puts "The line is currently:"
     katz_deli.each do |person|
-      puts "current place is #{counter}"
-      puts "name is #{person}"
+      puts "#{counter}. "
+      puts "#{person}"
       counter +=1
     end
   end
