9b3951f3e47360c8b19a77d193661ab6adf64cd8
Joey Cho <cjy12182000@naver.com>
2017-10-25 10:51:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else
+    counter = 1
+    katz_deli.each do |person|
+      puts "current place is #{counter}"
+      puts "name is #{person}"
+      counter +=1
+  end
+end
