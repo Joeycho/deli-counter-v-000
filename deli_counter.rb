fa12ec637ae69ad7dff5d35bb10563078d5fa228
Joey Cho <cjy12182000@naver.com>
2017-10-25 10:52:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     counter = 1
     katz_deli.each do |person|
